f173c691a6a8e5ff06aa8f4a91f4b8380b14ff3c
macnum <w.damilola@yahoo.com>
2024-08-13 10:13:09 +0100

    Add missing solution code for test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfRepeat) {
+    let stringRepeated ="";
+    if(numberOfRepeat < 0) return 'ERROR';
+    for(let i = numberOfRepeat; i > 0; i-- ) {
+        stringRepeated += string;
+    }
+    return stringRepeated
 };
-
 // Do not edit below this line
 module.exports = repeatString;
+

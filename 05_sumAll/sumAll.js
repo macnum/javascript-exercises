85b7e20b435b61fa1b91e6be0ac809672a0b2240
macnum <w.damilola@yahoo.com>
2024-08-14 13:48:35 +0100

    Add solution code to test case sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
+    let number = 0;
+    const largestNumber = Math.max(firstNumber, lastNumber);
+    const lowestNumber = Math.min(firstNumber, lastNumber);
 
+    if( (Number.isInteger(firstNumber) && Number.isInteger(lastNumber)) && (Math.sign(firstNumber) == 1 && Math.sign(lastNumber) ==1 ) ){
+        for(let i = lowestNumber;i <= largestNumber;i++){
+            number += i;
+            }
+            
+            return number; 
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line

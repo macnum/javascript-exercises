0d5b405dd412382402b1e9e9c5bc3235cdd8acb6
macnum <w.damilola@yahoo.com>
2024-09-03 13:46:34 +0100

    Add solution code of finding the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  /*  const age = (x) => (x.yearOfDeath || new Date().getFullYear()) - x.yearOfBirth;
+    const oldest = arr.sort((a, b) =>
+      age(a) > age(b) ? -1 : 1
+    );
+    return oldest[0]; */
 
+  const currentYear = new Date().getFullYear();
+  const oldest = arr.sort((a, b) => {
+    const last = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+    const next = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+    return next - last;
+  });
+  return oldest[0];
 };
 
 // Do not edit below this line

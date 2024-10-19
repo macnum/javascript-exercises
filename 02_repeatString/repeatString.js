8aeacb8618e791b154b08396f58edde47dc8f6cf
macnum <w.damilola@yahoo.com>
2024-10-19 14:20:47 +0100

    Add solution to repeated string test spec

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) return 'ERROR';
+  let repeatedStr = '';
+  for (let i = 0; i < num; i++) {
+    repeatedStr += string;
+  }
+  return repeatedStr;
 };
 
 // Do not edit below this line

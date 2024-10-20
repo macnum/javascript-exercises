64d26eec90e5eff3f70c753cb4c49f29b553b6ea
macnum <w.damilola@yahoo.com>
2024-10-20 13:55:48 +0100

    Add solution code to calculator test case

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (firstOperand, secondOperand) {
+  return firstOperand + secondOperand;
 };
 
-const subtract = function() {
-	
+const subtract = function (firstOperand, secondOperand) {
+  return firstOperand - secondOperand;
 };
 
-const sum = function() {
-	
+const sum = function (arrOfNumbers) {
+  return arrOfNumbers.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arrOfNumbers) {
+  return arrOfNumbers.reduce((acc, cur) => acc * cur, 1);
 };
 
-const power = function() {
-	
+const power = function (number, pow) {
+  return Math.pow(number, pow);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (n === 0) return 1;
+  let factorial = 1;
+  for (number; number > 0; number--) {
+    factorial *= number;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
@@ -29,5 +34,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };

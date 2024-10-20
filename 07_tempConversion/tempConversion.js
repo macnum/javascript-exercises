675fdd5f7e72db839ed4e7a3bbe9da0c3e29faf9
macnum <w.damilola@yahoo.com>
2024-10-20 11:41:44 +0100

    Add solution code to test case on temparature conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  const isCelsius = (f - 32) * (5 / 9);
+  return +isCelsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  const isFahrenheit = c * (9 / 5) + 32;
+  return +isFahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };

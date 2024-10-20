08f31dddb697224be0df95a1084831113b4ca5e7
macnum <w.damilola@yahoo.com>
2024-10-20 11:08:10 +0100

    Add solution to test case spec removed from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removeItem) {
+  return array.filter((item) => !removeItem.includes(item));
 };
 
 // Do not edit below this line

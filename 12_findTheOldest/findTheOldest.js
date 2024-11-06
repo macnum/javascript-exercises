2935d6749e7bf12f92d4e9c1db858d5355ae9fb4
macnum <w.damilola@yahoo.com>
2024-11-06 19:50:57 +0100

    Add solution code to test case findTheoldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  const sortedByOldest = arr.sort((a, b) => {
+    if (!a.yearOfDeath || !b.yearOfDeath) {
+      const currentYear = new Date().getFullYear();
+      a.yearOfDeath = b.yearOfDeath = currentYear;
+    }
+    const prev = a.yearOfDeath - a.yearOfBirth;
+    const next = b.yearOfDeath - b.yearOfBirth;
+    return next - prev;
+  });
+  const oldest = sortedByOldest[0];
+  return oldest;
 };
 
 // Do not edit below this line

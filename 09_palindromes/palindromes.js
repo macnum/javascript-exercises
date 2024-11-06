6ca59c3ed6767b66a668a5e170e7177cb0d4c3d8
macnum <w.damilola@yahoo.com>
2024-11-06 15:19:10 +0100

    Add solution code to test case palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const regexPattern = /[\w\d]/;
+  const backWords = str
+    .split('')
+    .reverse()
+    .filter((letter) => regexPattern.test(letter))
+    .join('')
+    .toLowerCase();
+  const word = str
+    .match(/[\w\d]/g)
+    .join('')
+    .toLowerCase();
+  return backWords === word;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+// "str1-1 s".match(/[\w\d]/g).reverse().join("")

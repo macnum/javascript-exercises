2cd1db01ca90b719a3eb23c74b18ff3ac723cd21
macnum <w.damilola@yahoo.com>
2024-11-06 17:44:31 +0100

    Add solution code to fibonacci test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  let a = 0;
+  let b = 1;
+  let c;
 
+  number = parseInt(number);
+  if (number < 0) return 'OOPS';
+  if (number === 0) return 0;
+  if (number === 1 && number === 2) return 1;
+  for (let i = 2; i <= number; i++) {
+    c = a;
+    a = b;
+    b = c + a;
+  }
+  return b;
 };
 
 // Do not edit below this line

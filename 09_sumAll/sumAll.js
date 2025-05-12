607452b9fb13fb03549d18129d52915648a8bc10
macnum <w.damilola@yahoo.com>
2025-05-12 01:36:47 +0100

    update: add code that sums up numbers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    if (min < 0 || max < 0) return "ERROR";
+    else if (Number.isInteger(min) && Number.isInteger(max)) {
+        if (min > max) {
+            let number = max;
+            max = min;
+            min = number;
+
+        }
+
+        let number = null;
+        for (min; min <= max; min++) {
+            number += min;
+        }
+        return number;
+
+    } else {
+        return "ERROR"
+    }
 
 };
 

9d73a4f3d8f4ad811bfb263ed762dd16621d005a
macnum <w.damilola@yahoo.com>
2025-05-19 22:45:41 +0100

    update: add code for palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+	const pattern = /\w/gi;
+	const arr = word
+		.toLowerCase()
+		.split('')
+		.filter((char) => char.match(pattern));
+	const palindrome = arr.join('');
+	const reversePalindrome = arr.reverse().join('');
+	return palindrome == reversePalindrome;
 };
-
 // Do not edit below this line
 module.exports = palindromes;

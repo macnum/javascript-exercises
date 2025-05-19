27fbb9589e3da30e2542b20aca988b5fd3ba9db5
macnum <w.damilola@yahoo.com>
2025-05-19 23:34:21 +0100

    update: add code solution to fibonacci sequence exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (fib) {
+	let a = 1;
+	let b = 1;
+	fib = parseInt(fib);
+	let sequence = null;
+	if (fib < 0) return 'OOPS';
+	if (fib == 0) return 0;
+	if (fib == 1) return 1;
+	for (let i = 2; i <= fib; i++) {
+		sequence = a + b;
+		a = b;
+		b = sequence;
+	}
+	return a;
 };
 
 // Do not edit below this line

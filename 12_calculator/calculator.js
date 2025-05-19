03f61e9d9ab27331af7591b95328c37c5ee31290
macnum <w.damilola@yahoo.com>
2025-05-19 22:10:46 +0100

    update:update: add code that performs calculator functions

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,33 +1,38 @@
-const add = function() {
-	
+const add = function (operand1, operand2) {
+	return operand1 + operand2;
 };
 
-const subtract = function() {
-	
+const subtract = function (operand1, operand2) {
+	return operand1 - operand2;
 };
 
-const sum = function() {
-	
+const sum = function (operandArr) {
+	return operandArr.reduce((initial, operand) => initial + operand, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (operandArr) {
+	return operandArr.reduce((initial, operand) => initial * operand, 1);
 };
 
-const power = function() {
-	
+const power = function (operand, pow) {
+	return operand ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function (factorial) {
+	let sum = 1;
+	while (factorial > 0) {
+		sum *= factorial;
+		factorial--;
+	}
+	return sum;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };

ed842520f974a547ff7440c7d326ae9fb576966c
macnum <w.damilola@yahoo.com>
2025-05-20 08:59:01 +0100

    update: add code that finds the oldest in an array of objects

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (oldest) {
+	let currentYear = new Date().getFullYear();
+	oldest.sort((a, b) => {
+		const prev = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+		const next = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+		return prev > next ? -1 : 1;
+	});
+	return oldest[0];
 };
 
 // Do not edit below this line

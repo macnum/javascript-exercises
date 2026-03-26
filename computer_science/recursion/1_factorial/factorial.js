b21b9cebcc626784122855b71c4d6fbd4f75965b
Macnum <w.damilola@yahoo.com>
2026-03-26 12:39:43 +0100

    feat: add code forfactorail recursion test

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,8 @@
-const factorial = function() {
+const factorial = function (n) {
+	if (n === 0) return 1;
+	if (!Number.isInteger(n) || !(n > 0)) return undefined;
 
+	return n * factorial(n - 1);
 };
 
 // Do not edit below this line

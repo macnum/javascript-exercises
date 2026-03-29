455aa5f2a13e1314145cc3808c9f601498def5e6
Macnum <w.damilola@yahoo.com>
2026-03-29 18:53:00 +0100

    feat: add code for length of total integers recursion test

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,21 @@
-const totalIntegers = function() {
-  
+const isObject = (value) => typeof value === 'object' && value !== null;
+
+const totalIntegers = function (obj) {
+	if (!isObject(obj)) return;
+	let total = 0;
+
+	const element = Object.values(obj);
+
+	for (let el of element) {
+		if (Number.isInteger(el)) {
+			++total;
+		} else if (isObject(el)) {
+			total += totalIntegers(el);
+		}
+	}
+
+	return total;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;

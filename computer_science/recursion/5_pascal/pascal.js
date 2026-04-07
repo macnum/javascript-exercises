2954e76ca4d848fc6dc19baec0c0dbff876f0c4d
Macnum <w.damilola@yahoo.com>
2026-04-07 20:20:49 +0100

    feat: add code for all recursion test

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,20 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+	if (n === 1) {
+		return [1];
+	}
+
+	const prevRow = pascal(n - 1);
+
+	const newRow = [];
+
+	newRow.push(1);
+
+	for (let i = 0; i < prevRow.length - 1; i++) {
+		newRow.push(prevRow[i] + prevRow[i + 1]);
+	}
+	newRow.push(1);
+	return newRow;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
